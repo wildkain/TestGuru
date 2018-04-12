b96cd3e1918c450f2fd120c4fd33fa4d46d8ba44
Stanislav <wildkain@gmail.com>
2018-04-12 23:35:16 +0300

    Create assotiation User Test (through: TestsProgress)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,7 @@
 class Test < ApplicationRecord
+has_many :tests_progresses
+has_many :users, through: :tests_progresses
+
   def self.get_titles(category_name)
     Test.joins("JOIN categories ON categories.id = tests.category_id").where("categories.title = ?", category_name ).order(title: :desc).pluck(:title)
   end
---
+++ app/models/tests_progress.rb
@@ -0,0 +1,4 @@
+class TestsProgress < ApplicationRecord
+  belongs_to :test
+  belongs_to :user
+end
