35b56e3cd99324b2dc72e1c9c6062f74d254d57e
Stanislav <wildkain@gmail.com>
2018-04-13 15:42:28 +0300

    Scopes and Validations

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,8 +6,17 @@ has_many :categories, through: :categorizations
 has_many :questions
 belongs_to :author, class_name: "User", foreign_key: "author_id"
 
+scope :easy,   -> { where(level: 0..1) }
+scope :medium, -> { where(level: 2..4) }
+scope :hard,   -> { where(level: 5..Float::INFINITY) }
+scope :with_level, (level) -> { where(level: level ) }
+scope :by_category, ->(category) {  joins(:category).where(categories: { title: category }).order(title: :desc) }
+
+validates :title, :level, presence: true,
+                          uniqueness: true
+validates :level, numericality: { only_integer: true, greater_than: 0 }
 
   def self.get_titles(category_name)
-    Test.categories.where(category.title = caegory_name).order(title: :desc).pluck(:title)
+    by_category(category_name).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
 has_many :tests_progresses
 has_many :tests, through: :tests_progresses
 
+validates :email, presence: true,
+                  uniqueness: true
 
   def show_tests_with_level(level_value)
-    self.tests.where(level: level_value )
+    self.tests.with_level(level_value)
   end
 end
