361dff30f6f36fd5d6f7b730446bca6f605c3668
Stanislav <wildkain@gmail.com>
2018-04-18 16:43:20 +0300

    hotfix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,23 +6,25 @@ class QuestionsController < ApplicationController
 
   def index
     @questions = @test.questions.all
+    render inline: "<% @questions.each do |q|%><p><%= q.body %></p><%end%>"
   end
 
   def show
-    render plain: 'Single question'
-  end
-
-  def new
-    @question = @test.question.new
+    render inline: "<h2><%=@question.body%></h2>"
   end
 
   def create
-    question = @tets.questions.create(questions_params)
+    question = @test.questions.create(question_params)
     render html: '<h1> Question created!</h1>'.html_safe
   end
 
   def destroy
     @question.destroy
+    redirect_to :index
+  end
+
+  def new
+    @question = @test.questions.new
   end
 
   private
