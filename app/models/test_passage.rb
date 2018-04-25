6428d0d70014e7744aeed37d65249ba609c52e22
Stanislav <wildkain@gmail.com>
2018-04-25 20:17:08 +0300

    Homework (add callbacks to TestPassage, edit views)

---
+++ app/models/test_passage.rb
@@ -0,0 +1,61 @@
+class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: 'Question', foreign_key: 'current_question_id', optional: true
+
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_update :go_next_question, on: :accept!
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+    end
+    save!
+
+  end
+
+  def go_next_question
+    self.current_question = next_question.first
+  end
+
+  def get_percent(test_passage)
+   ((test_passage.correct_questions * 100) / test_passage.test.questions.count).round(3)
+  end
+
+  def current_question_number
+    number_of_questions - next_question.count
+  end
+
+  def number_of_questions
+    test.questions.count
+  end
+
+  private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+        correct_answers_count == answer_ids.count
+
+  end
+
+
+  def correct_answers
+    current_question.answers.correct
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id)
+  end
+
+end
