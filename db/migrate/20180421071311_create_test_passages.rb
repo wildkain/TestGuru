ed2a93e395aa1fbfe30e6636cf988288eb84a7c4
Stanislav <wildkain@gmail.com>
2018-05-15 00:39:26 +0300

    faildeploy5

--- db/migrate/20180421071311_create_test_passages.rb
+++ db/migrate/20180421071311_create_test_passages.rb
@@ -3,7 +3,7 @@ class CreateTestPassages < ActiveRecord::Migration[5.1]
     create_table :test_passages do |t|
       t.references :user, foreign_key: true
       t.references :test, foreign_key: true
-      t.references :current_question, foreign_key: true
+      t.references :current_question_id, foreign_key: true
       t.integer :correct_questions, default: 0
 
       t.timestamps
