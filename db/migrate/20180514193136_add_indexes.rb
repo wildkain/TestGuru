41137ca13720c288b290d291c592954c5d2bb58a
Stanislav <wildkain@gmail.com>
2018-05-14 23:38:15 +0300

    Postgre

---
+++ db/migrate/20180514193136_add_indexes.rb
@@ -0,0 +1,6 @@
+class AddIndexes < ActiveRecord::Migration[5.1]
+  def change
+    add_index :tests, :author_id
+    add_index :tests, %i[title level], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509131518) do
+ActiveRecord::Schema.define(version: 20180514193136) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -72,7 +75,9 @@ ActiveRecord::Schema.define(version: 20180509131518) do
     t.datetime "updated_at", null: false
     t.integer "category_id"
     t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
