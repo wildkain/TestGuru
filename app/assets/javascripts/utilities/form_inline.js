eae0a4ed775684faa4ba90f95c70a34cfed4f54e
Stanislav <wildkain@gmail.com>
2018-05-14 20:41:36 +0300

    fixstyle

--- app/assets/javascripts/utilities/form_inline.js
+++ app/assets/javascripts/utilities/form_inline.js
@@ -1,38 +1,33 @@
 document.addEventListener('turbolinks:load', function () {
-   $('.form-inline-link').on('click', formInlineLinkHandler)
+   $('.form-inline-link').on('click', formInlineLinkHandler);
 
-
-
-    var errors = document.querySelector('.resource-errors')
+    var errors = document.querySelector('.resource-errors');
 
     if (errors) {
-        var resourceId = errors.dataset.resourceId
+        var resourceId = errors.dataset.resourceId;
         formInlineHandler(resourceId)
     }
 
-})
+});
 
 function formInlineLinkHandler(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    var testId = this.dataset.testId
+    var testId = this.dataset.testId;
     formInlineHandler(testId)
 }
 
-
 function formInlineHandler(testId) {
-    var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
-    var $formInline = $('.form-inline[data-test-id="'+ testId + '"]')
-    var $link = $('.form-inline-link[data-test-id="' + testId + '"]')
+    var $testTitle = $('.test-title[data-test-id="' + testId + '"]');
+    var $formInline = $('.form-inline[data-test-id="'+ testId + '"]');
+    var $link = $('.form-inline-link[data-test-id="' + testId + '"]');
 
-
-    $formInline.toggle()
-    $testTitle.toggle()
+    $formInline.toggle();
+    $testTitle.toggle();
 
     if ($formInline.is(':visible')){
       $link.text('Cancel')
     } else {
       $link.text('Edit')
     }
-
 }
