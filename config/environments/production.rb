18d007e69cf20a352ba93aa0f3f9774d600c0e40
Stanislav <wildkain@gmail.com>
2018-05-15 09:10:25 +0300

    ActionMAiler

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'gentle-shelf-11626.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      username:             ENV['SMTP_USERNAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentification:     'plain',
+      enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
