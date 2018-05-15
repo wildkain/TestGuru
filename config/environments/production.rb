8fdd6bca8b6edbfe818c5e63fef3d909845f0bfb
Stanislav <wildkain@gmail.com>
2018-05-15 11:47:14 +0300

    portchange

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 465,
+      port:                 587,
       user_name:             ENV['SMTP_USERNAME'],
       password:             ENV['SMTP_PASSWORD'],
       authentification:     'plain',
